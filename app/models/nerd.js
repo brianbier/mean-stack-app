4c1bd737e55e972ce058d94e78abf6c62df4976a
Brian Bier <briansbier@gmail.com>
2016-06-29 10:09:05 -0400

    Intial backend set up

---
+++ app/models/nerd.js
@@ -0,0 +1,19 @@
+// grab the mongoose module
+
+var mongoose = require('mongoose');
+
+// Creating the schema
+var nerdSchema = mongoose.schema(
+  {
+  name: {type: String,default: ''}
+  },
+  {
+    timestamps: true
+  }
+);
+
+//define our nerd model
+// module.exports allows us to pass this to other files when it is called
+var Nerd = mongoose.model('Nerd',nerdSchema);
+
+module.exports = Nerd
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: 'mongodb://bbiereyes:password@ds023714.mlab.com:23714/mean'
+}
